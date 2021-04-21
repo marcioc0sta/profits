564c7d91e55d00d834abdc3707a75bb87cc099ef
marcioc0sta <marciocst2@gmail.com>
2021-04-21 18:35:38 -0300

    fix: test_round lint

--- app/tests/helpers/test_round.py
+++ app/tests/helpers/test_round.py
@@ -2,7 +2,7 @@ from app.helpers.round import round_helper
 
 
 def describe_round_helper():
-    def should_return_a_rounded_number_with_default_decimal_count(num_to_round):
+    def return_a_rounded_num_with_default_decimal_count(num_to_round):
         # given
         expected_result = 11.23
         # when
@@ -10,7 +10,7 @@ def describe_round_helper():
         # then
         assert rounded_num == expected_result
 
-    def should_return_a_rounded_number_accordig_with_given_decimal_count(num_to_round):
+    def return_a_rounded_num_accordig_with_given_decimal_count(num_to_round):
         # given
         expected_result = 11.2
         # when
