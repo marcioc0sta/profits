6b1df9fb1f88787667fe5973e4495794364edf99
marcioc0sta <marciocst2@gmail.com>
2021-04-21 09:30:05 -0300

    refactor: formatting with black

--- app/apis/goals.py
+++ app/apis/goals.py
@@ -3,13 +3,14 @@ from app.helpers.round import round_helper
 
 
 def get_goals(goal):
-    raw_value = npf.rate(goal.time, -goal.montly_contribution, -
-                         goal.initial_contribution, goal.goal_value)
+    raw_value = npf.rate(
+        goal.time,
+        -goal.montly_contribution,
+        -goal.initial_contribution,
+        goal.goal_value,
+    )
 
     monthly = round_helper(raw_value)
     yearly = round(pow(12 - 1, monthly), 2)
 
-    return {
-        "monthly": monthly,
-        "yearly": yearly
-    }
+    return {"monthly": monthly, "yearly": yearly}
--- app/routes/views.py
+++ app/routes/views.py
@@ -6,9 +6,9 @@ from app.apis.goals import get_goals
 router = APIRouter()
 
 
-@router.post('/profitability-goals', tags=['/profitability-goals'])
+@router.post("/profitability-goals", tags=["/profitability-goals"])
 async def profitability_goals(goal: Goal):
     return {
-        "Monthly goal": get_goals(goal)['monthly'],
-        "Anual goal": get_goals(goal)['yearly']
+        "Monthly goal": get_goals(goal)["monthly"],
+        "Anual goal": get_goals(goal)["yearly"],
     }
